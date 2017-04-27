f0e36539beaf216e689f5348f50da331187ecfaf
gudlarakesh <gudlarakesh1991@gmail.com>
2017-04-27 20:29:07 +0530

    Added keywords for every book model in server.js

--- server.js
+++ server.js
@@ -19,10 +19,15 @@ app.configure(function() {
 
 mongoose.connect('mongodb://localhost/library_database');
 
+var Keywords = new mongoose.Schema({
+	keyword: String
+});
+
 var Book = new mongoose.Schema({
 	title: String,
 	author: String,
-	releaseDate: Date
+	releaseDate: Date,
+	keywords: [ Keywords ]
 });
 
 var BookModel = mongoose.model('Book',Book);
@@ -51,7 +56,8 @@ app.post('/api/books', function(request, response) {
 	var book = new BookModel({
 		title: request.body.title,
 		author: request.body.author,
-		releaseDate: request.body.releaseDate
+		releaseDate: request.body.releaseDate,
+		keywords: request.body.keywords
 	});
 	book.save(function(err) {
 		if(!err) {
@@ -79,6 +85,7 @@ app.put('/api/books/:id', function(request, response) {
 		book.title = request.body.title;
 		book.author = request.body.author;
 		book.releaseDate = request.body.releaseDate;
+		book.keywords = request.body.keywords;
 
 		return book.save(function(err) {
 			if(!err) {
